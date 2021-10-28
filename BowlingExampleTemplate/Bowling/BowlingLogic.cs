437b0d9e43e89b7738f12ca032dc3a7c412472c8
Andrija Pavlov <pavlovandrija9@gmail.com>
2021-10-28 18:34:15 +0200

    Copying bowling game project.

---
+++ BowlingExampleTemplate/Bowling/BowlingLogic.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingExampleTemplate.Bowling
+{
+    public class BowlingLogic
+    {
+
+		int[] throws = new int[21];
+		int currentThrow;
+
+		public void ThrowBall(int pins)
+		{
+			throws[currentThrow] = pins;
+			currentThrow++;
+		}
+
+		public int CheckScore()
+		{
+
+			int score = 0;
+			int frame = 0;
+
+			for (int i = 0; i < 10; i++)
+			{
+
+				if (IsSpare(frame))
+				{
+					score += 10 + SpareBonus(frame);
+					frame += 2;
+
+				}
+				else if (IsStrike(frame))
+				{
+					score += 10 + StrikeBonus(frame);
+					frame += 1;
+				}
+				else
+				{
+					score += SumPins(frame);
+					frame += 2;
+				}
+			}
+
+			return score;
+		}
+
+
+
+		public bool IsSpare(int frame)
+		{
+			return SumPins(frame) == 10;
+		}
+
+		public bool IsStrike(int frame)
+		{
+			return throws[frame] == 10;
+		}
+
+		public int SumPins(int frame)
+		{
+			return throws[frame] + throws[frame + 1];
+		}
+
+		public int SpareBonus(int frame)
+		{
+			return throws[frame + 2];
+		}
+
+		public int StrikeBonus(int frame)
+		{
+			return throws[frame + 1] + throws[frame + 2];
+		}
+	}
+}
---
+++ BowlingExampleTemplate/Program.cs
@@ -0,0 +1,26 @@
+﻿using BowlingExampleTemplate.Bowling;
+using System;
+
+namespace BowlingExampleTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BowlingLogic bowling = new BowlingLogic();
+            int result = TryPerfectGame(bowling);
+            Console.WriteLine("Score: " + result);
+        }
+
+        static int TryPerfectGame(BowlingLogic bowling)
+        {
+            int score = 0;
+            for(int i = 0; i<12; i++)
+            {
+                bowling.ThrowBall(10);
+            }
+            score = bowling.CheckScore();
+            return score;
+        }
+    }
+}
